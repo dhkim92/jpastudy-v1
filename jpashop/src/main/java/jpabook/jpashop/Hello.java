efb8802102546a591368b23927f1e490bd5b7ee4
dhkim92 <dhkim9294@gmail.com>
2021-07-06 13:11:45 +0900

    first commit
    inflearn jpa study v1

---
+++ jpashop/src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop;
+
+public class Hello {
+
+
+
+}
---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        var savedId = memberRepository.save(member);
+        var findMember = memberRepository.find(savedId);
+
+        // then
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+
+    }
+}
